753c6267db70349c40accc1b446b1c7024fc7959
keshakremov23 <lunxart3@gmail.com>
2025-09-26 23:33:02 +0300

    сделал курсы с площадками+подвязал, оформил main page

--- bazaznaniymgkeit1/main/urls.py
+++ bazaznaniymgkeit1/main/urls.py
@@ -3,9 +3,24 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about'),
     path('place1', views.place1, name='place1'),
     path('place2', views.place2, name='place2'),
     path('place3', views.place3, name='place3'),
-    path('place4', views.place4, name='place4')
+    path('place4', views.place4, name='place4'),
+    path('k1p1', views.k1p1, name='k1p1'),
+    path('k2p1', views.k2p1, name='k2p1'),
+    path('k3p1', views.k3p1, name='k3p1'),
+    path('k4p1', views.k4p1, name='k4p1'),
+    path('k1p2', views.k1p2, name='k1p2'),
+    path('k2p2', views.k2p2, name='k2p2'),
+    path('k3p2', views.k3p2, name='k3p2'),
+    path('k4p2', views.k4p2, name='k4p2'),
+    path('k1p3', views.k1p3, name='k1p3'),
+    path('k2p3', views.k2p3, name='k2p3'),
+    path('k3p3', views.k3p3, name='k3p3'),
+    path('k4p3', views.k4p3, name='k4p3'),
+    path('k1p4', views.k1p4, name='k1p4'),
+    path('k2p4', views.k2p4, name='k2p4'),
+    path('k3p4', views.k3p4, name='k3p4'),
+    path('k4p4', views.k4p4, name='k4p4'),
 ]
--- bazaznaniymgkeit1/main/views.py
+++ bazaznaniymgkeit1/main/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'main/index.html')
 
-def about(request):
-    return render(request, 'main/about.html')
-
 def place1(request):
     return render(request, 'main/place1.html')
 
@@ -19,4 +16,53 @@ def place3(request):
 
 def place4(request):
     return render(request, 'main/place4.html')
+
+def k1p1(request):
+    return render(request, 'main/p1/k1p1.html')
+
+def k2p1(request):
+    return render(request, 'main/p1/k2p1.html')
+
+def k3p1(request):
+    return render(request, 'main/p1/k3p1.html')
+
+def k4p1(request):
+    return render(request, 'main/p1/k4p1.html')
+
+def k1p2(request):
+    return render(request, 'main/p2/k1p2.html')
+
+def k2p2(request):
+    return render(request, 'main/p2/k2p2.html')
+
+def k3p2(request):
+    return render(request, 'main/p2/k3p2.html')
+
+def k4p2(request):
+    return render(request, 'main/p3/k4p2.html')
+
+def k1p3(request):
+    return render(request, 'main/p3/k1p3.html')
+
+def k2p3(request):
+    return render(request, 'main/p3/k2p3.html')
+
+def k3p3(request):
+    return render(request, 'main/p3/k3p3.html')
+
+def k4p3(request):
+    return render(request, 'main/p3/k4p3.html')
+
+def k1p4(request):
+    return render(request, 'main/p4/k1p4.html')
+
+def k2p4(request):
+    return render(request, 'main/p4/k2p4.html')
+
+def k3p4(request):
+    return render(request, 'main/p4/k3p4.html')
+
+def k4p4(request):
+    return render(request, 'main/p4/k4p4.html')
+
 
