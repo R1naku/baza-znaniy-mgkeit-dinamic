ee6ef6bffae279307e4665fe87272ecb444ddfc3
keshakremov23 <lunxart3@gmail.com>
2025-09-27 09:49:00 +0300

    сделал курсы с площадками+подвязал, оформил main page

--- bazaznaniymgkeit1/main/views.py
+++ bazaznaniymgkeit1/main/views.py
@@ -65,4 +65,194 @@ def k3p4(request):
 def k4p4(request):
     return render(request, 'main/p4/k4p4.html')
 
+def l1k1p1(request):
+    return render(request, 'main/p1/lessonk1p1/l1k1p1.html')
 
+def l2k1p1(request):
+    return render(request, 'main/p1/lessonk1p1/l2k1p1.html')
+
+def l3k1p1(request):
+    return render(request, 'main/p1/lessonk1p1/l3k1p1.html')
+
+def l4k1p1(request):
+    return render(request, 'main/p1/lessonk1p1/l4k1p1.html')
+
+def l1k2p1(request):
+    return render(request, 'main/p1/lessonk2p1/l1k2p1.html')
+
+def l2k2p1(request):
+    return render(request, 'main/p1/lessonk2p1/l2k2p1.html')
+    
+def l3k2p1(request):
+    return render(request, 'main/p1/lessonk2p1/l3k2p1.html')
+
+def l4k2p1(request):
+    return render(request, 'main/p1/lessonk2p1/l4k2p1.html')
+
+def l1k3p1(request):
+    return render(request, 'main/p1/lessonk3p1/l1kp1.html')
+
+def l2k3p1(request):
+    return render(request, 'main/p1/lessonk3p1/l2k3p1.html')
+    
+def l3k3p1(request):
+    return render(request, 'main/p1/lessonk3p1/l3k3p1.html')
+
+def l4k3p1(request):
+    return render(request, 'main/p1/lessonk3p1/l4k3p1.html')
+
+def l1k4p1(request):
+    return render(request, 'main/p1/lessonk4p1/l1k4p1.html')
+
+def l2k4p1(request):
+    return render(request, 'main/p1/lessonk4p1/l2k4p1.html')
+    
+def l3k4p1(request):
+    return render(request, 'main/p1/lessonk4p1/l3k4p1.html')
+
+def l4k4p1(request):
+    return render(request, 'main/p1/lessonk4p1/l4k4p1.html')
+
+def l1k1p2(request):
+    return render(request, 'main/p2/lessonk1p2/l1k1p2.html')
+
+def l2k1p2(request):
+    return render(request, 'main/p2/lessonk1p2/l2k1p2.html')
+
+def l3k1p2(request):
+    return render(request, 'main/p2/lessonk1p2/l3k1p2.html')
+
+def l4k1p2(request):
+    return render(request, 'main/p2/lessonk1p2/l4k1p2.html')
+
+def l1k2p2(request):
+    return render(request, 'main/p2/lessonk2p2/l1k2p2.html')
+
+def l2k2p2(request):
+    return render(request, 'main/p2/lessonk2p2/l2k2p2.html')
+    
+def l3k2p2(request):
+    return render(request, 'main/p2/lessonk2p2/l3k2p2.html')
+
+def l4k2p2(request):
+    return render(request, 'main/p2/lessonk2p2/l4k2p2.html')
+
+def l1k3p2(request):
+    return render(request, 'main/p2/lessonk3p2/l1k3p2html')
+
+def l2k3p2(request):
+    return render(request, 'main/p2/lessonk3p2/l2k3p2.html')
+    
+def l3k3p2(request):
+    return render(request, 'main/p2/lessonk3p2/l3k3p2.html')
+
+def l4k3p2(request):
+    return render(request, 'main/p2/lessonk3p2/l4k3p2.html')
+
+def l1k4p2(request):
+    return render(request, 'main/p2/lessonk4p2/l1k4p2.html')
+
+def l2k4p2(request):
+    return render(request, 'main/p2/lessonk4p2/l2k4p2.html')
+    
+def l3k4p2(request):
+    return render(request, 'main/p2/lessonk4p2/l3k4p2.html')
+
+def l4k4p2(request):
+    return render(request, 'main/p2/lessonk4p2/l4k4p2.html')
+
+def l1k1p3(request):
+    return render(request, 'main/p3/lessonk1p3/l1k1p3.html')
+
+def l2k1p3(request):
+    return render(request, 'main/p3/lessonk1p3/l2k1p3.html')
+
+def l3k1p3(request):
+    return render(request, 'main/p3/lessonk1p3/l3k1p3.html')
+
+def l4k1p3(request):
+    return render(request, 'main/p3/lessonk1p3/l4k1p3.html')
+
+def l1k2p3(request):
+    return render(request, 'main/p3/lessonk2p3/l1k2p3.html')
+
+def l2k2p3(request):
+    return render(request, 'main/p3/lessonk2p3/l2k2p3.html')
+    
+def l3k2p3(request):
+    return render(request, 'main/p3/lessonk2p3/l3k2p3.html')
+
+def l4k2p3(request):
+    return render(request, 'main/p3/lessonk2p3/l4k2p3.html')
+
+def l1k3p3(request):
+    return render(request, 'main/p3/lessonk3p3/l1k3p3.html')
+
+def l2k3p3(request):
+    return render(request, 'main/p3/lessonk3p3/l2k3p3.html')
+    
+def l3k3p3(request):
+    return render(request, 'main/p3/lessonk3p3/l3k3p3.html')
+
+def l4k3p3(request):
+    return render(request, 'main/p3/lessonk3p3/l4k3p3.html')
+
+def l1k4p3(request):
+    return render(request, 'main/p3/lessonk4p3/l1k4p3.html')
+
+def l2k4p3(request):
+    return render(request, 'main/p3/lessonk4p3/l2k4p3.html')
+    
+def l3k4p3(request):
+    return render(request, 'main/p3/lessonk4p3/l3k4p3.html')
+
+def l4k4p3(request):
+    return render(request, 'main/p3/lessonk4p3/l4k4p3.html')
+
+def l1k1p4(request):
+    return render(request, 'main/p4/lessonk1p4/l1k1p4.html')
+
+def l2k1p4(request):
+    return render(request, 'main/p4/lessonk1p4/l2k1p4.html')
+
+def l3k1p4(request):
+    return render(request, 'main/p4/lessonk1p4/l3k1p4.html')
+
+def l4k1p4(request):
+    return render(request, 'main/p4/lessonk1p4/l4k1p4.html')
+
+def l1k2p4(request):
+    return render(request, 'main/p4/lessonk2p4/l1k2p4.html')
+
+def l2k2p4(request):
+    return render(request, 'main/p4/lessonk2p4/l2k2p4.html')
+    
+def l3k2p4(request):
+    return render(request, 'main/p4/lessonk2p4/l3k2p4.html')
+
+def l4k2p4(request):
+    return render(request, 'main/p4/lessonk2p4/l4k2p4.html')
+
+def l1k3p4(request):
+    return render(request, 'main/p4/lessonk3p4/l1kp4html')
+
+def l2k3p4(request):
+    return render(request, 'main/p4/lessonk3p4/l2k3p4.html')
+    
+def l3k3p4(request):
+    return render(request, 'main/p4/lessonk3p4/l3k3p4.html')
+
+def l4k3p4(request):
+    return render(request, 'main/p4/lessonk3p4/l4k3p4.html')
+
+def l1k4p4(request):
+    return render(request, 'main/p4/lessonk4p4/l1k4p4.html')
+
+def l2k4p4(request):
+    return render(request, 'main/p4/lessonk4p4/l2k4p4.html')
+    
+def l3k4p4(request):
+    return render(request, 'main/p4/lessonk4p4/l3k4p4.html')
+
+def l4k4p4(request):
+    return render(request, 'main/p4/lessonk4p4/l4k4p4.html')
